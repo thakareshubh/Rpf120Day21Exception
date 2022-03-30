009026d7ab7d01e1214c0a80b355259f26b6827b
thakareshubh <shubhamthakare032@gmail.com>
2022-03-30 14:28:58 +0530

    Added exception code

---
+++ MoodAnalyserException/MoodAnalyserException/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MoodAnalyserException
+{
+    internal class Program
+    {
+        public  int CheckMood (string mood,int e)
+        {
+            try
+            {
+                int i = 0;
+                int j = 1;
+                string check = "Happy";
+                if (mood == check)
+                {
+                    Console.WriteLine("person is happy");
+                }
+                else
+                {
+                    return j / i;
+
+                }
+                return j / i;
+            }catch (Exception e)
+            {
+                return e;   
+            }
+            }
+            
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Person mood");
+            string mood1 = Convert.ToString(10); 
+            Program check=new Program();
+            check.CheckMood(mood1);
+        }
+    }
+}
